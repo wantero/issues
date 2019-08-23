357c0764cec0f914f6313beb215e589d3a194ec9
William Pavei Antero <williamantero@gmail.com>
2019-08-23 17:05:20 -0300

    refactory CLI module

--- lib/issues/cli.ex
+++ lib/issues/cli.ex
@@ -22,13 +22,21 @@ defmodule Issues.CLI do
   """
 
   def parse_args(argv) do
-    parse = OptionParser.parse(argv, switches: [help: :boolean], aliases: [h: :help])
+    OptionParser.parse(argv, switches: [help: :boolean], aliases: [h: :help])
+    |> elem(1)
+    |> args_to_internal_representation()
+  end
 
-    case parse do
-      {[help: true], _, _} -> :help
-      {_, [user, project, count], _} -> {user, project, String.to_integer(count)}
-      {_, [user, project], _} -> {user, project, @default_count}
-      _ -> :help
-    end
+  def args_to_internal_representation([user, project, count]) do
+    {user, project, String.to_integer(count)}
+  end
+
+  def args_to_internal_representation([user, project]) do
+    {user, project, @default_count}
+  end
+
+  # bad arg or --help
+  def args_to_internal_representation(_) do
+    :help
   end
 end
