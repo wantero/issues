c22839b5404145204d204cb9ab6dcae2d30970ac
William Pavei Antero <williamantero@gmail.com>
2019-09-28 12:24:16 -0300

    Sort data

--- lib/issues/cli.ex
+++ lib/issues/cli.ex
@@ -51,7 +51,9 @@ defmodule Issues.CLI do
   end
 
   def process({user, project, _count}) do
-    Issues.GithubIssues.fetch(user, project) |> decode_response()
+    Issues.GithubIssues.fetch(user, project)
+    |> decode_response()
+    |> sort_into_descending_order()
   end
 
   def decode_response({:ok, body}) do
@@ -61,5 +63,12 @@ defmodule Issues.CLI do
   def decode_response({:error, error}) do
     IO.puts("Error fetching from Github: #{error["message"]}")
     System.halt(2)
+  end
+
+  def sort_into_descending_order(list_of_issues) do
+    list_of_issues
+    |> Enum.sort(fn i1, i2 ->
+      i1["created_at"] >= i2["created_at"]
+    end)
   end
 end
