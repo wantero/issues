902b958097c505862dc015c9577b720b3b17511e
William Pavei Antero <williamantero@gmail.com>
2019-09-28 12:33:06 -0300

    Take first n items

--- lib/issues/cli.ex
+++ lib/issues/cli.ex
@@ -50,10 +50,11 @@ defmodule Issues.CLI do
     System.halt(0)
   end
 
-  def process({user, project, _count}) do
+  def process({user, project, count}) do
     Issues.GithubIssues.fetch(user, project)
     |> decode_response()
     |> sort_into_descending_order()
+    |> last(count)
   end
 
   def decode_response({:ok, body}) do
@@ -70,5 +71,11 @@ defmodule Issues.CLI do
     |> Enum.sort(fn i1, i2 ->
       i1["created_at"] >= i2["created_at"]
     end)
+  end
+
+  def last(list, count) do
+    list
+    |> Enum.take(count)
+    |> Enum.reverse()
   end
 end
